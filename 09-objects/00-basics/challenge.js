b3d5c11cfc9c17490d3ccd718727c6192789e890
Jake Rorrer <jdrorrer@gmail.com>
2015-01-27 22:16:50 +0000

    Completed JS objects exercise.

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,32 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
+  return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (object[newProp]) {
+    return object;
+  }
+  else {
+    object[newProp] = newValue;
+    return object;
+  }
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" +letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  var priceTable = {
+    'Apple Sticker': 1,
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499
+  };
+  return money >= priceTable[item];
+};
