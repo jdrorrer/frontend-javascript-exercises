6d075c66748c39cf6b44c8f08f223d48777f0bfc
Jake Rorrer <jdrorrer@gmail.com>
2015-01-27 15:04:16 +0000

    Completed JS Strings exercises.

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,23 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return "Hello " + firstName + ",\n\n" + message + "\n\n" + "Sincerely,\n" + senderName;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  var newString = bigString.substring(startA, endA) + bigString.substring(startB, endB);
+  return newString;
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+  var searchIndex = text.indexOf(searchString);
+  var searchLastIndex = text.lastIndexOf(searchString);
+  var searchLength = searchString.length;
+  return text.substring(searchIndex + searchLength,searchLastIndex);
 };
